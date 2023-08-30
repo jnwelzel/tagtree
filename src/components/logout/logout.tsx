d50dd307357f9d0687e0110ae6b9afa2a49175fa
Jonathan Welzel <jnwelzel@gmail.com>
2023-08-30 09:22:36 +0200

    LP improvs

--- src/components/logout/logout.tsx
+++ src/components/logout/logout.tsx
@@ -1,4 +1,3 @@
-import { component$ } from "@builder.io/qwik";
 import { globalAction$ } from "@builder.io/qwik-city";
 import CookiesEnum from "~/utils/CookiesEnum";
 
@@ -6,10 +5,4 @@ export const useLogout = globalAction$((_data, { cookie, redirect }) => {
   cookie.delete(CookiesEnum.Session, { path: "/" });
 
   throw redirect(308, "/");
-});
-
-export default component$(() => {
-  const action = useLogout();
-
-  return <button onClick$={() => action.submit()}>Log out</button>;
 });
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -18,43 +18,45 @@ export default component$(() => {
 
   return (
     <div class="flex flex-col min-h-full text-center">
-      <div class="flex-1">
-        <div class="flex items-center p-4 justify-end border-b border-gray-300">
-          {!signal.value.isLoggedIn && (
-            <>
-              <MyLink href="/login" class="mr-3">
-                Login
-              </MyLink>
-              <Link
-                href="/signup"
-                class="rounded-full px-5 py-3 bg-violet-400 text-white"
-              >
-                Create an account
-              </Link>
-            </>
-          )}
-          {signal.value.isLoggedIn && (
-            <>
-              <MyLink href="/admin" class="mr-3">
-                Admin
-              </MyLink>
-              <button
-                onClick$={() => logoutAction.submit()}
-                class="rounded-full px-5 py-3 bg-violet-400 text-white"
-              >
-                Log out
-              </button>
-            </>
-          )}
-        </div>
+      <div class="flex items-center p-4 justify-end border-b border-gray-300 bg-gray-100 fixed top-0 left-0 right-0">
+        {!signal.value.isLoggedIn && (
+          <>
+            <MyLink href="/login" class="mr-3">
+              Login
+            </MyLink>
+            <Link
+              href="/signup"
+              class="rounded-full px-5 py-3 bg-violet-400 text-white"
+            >
+              Create an account
+            </Link>
+          </>
+        )}
+        {signal.value.isLoggedIn && (
+          <>
+            <MyLink href="/admin" class="mr-3">
+              Admin
+            </MyLink>
+            <button
+              onClick$={() => logoutAction.submit()}
+              class="rounded-full px-5 py-3 bg-violet-400 text-white"
+            >
+              Log out
+            </button>
+          </>
+        )}
       </div>
+
+      <div class="flex-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500" />
+
       <div class="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 py-9 text-white flex-1 flex flex-col items-center justify-center relative tagtree-gradient">
         <h1 class="text-7xl md:text-9xl tagtree-font drop-shadow-md">
           Tagtree
         </h1>
         <h2 class="text-xl drop-shadow">All your gamer tags in one place.</h2>
       </div>
-      <div class="flex-1">footer</div>
+
+      <div class="flex-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500" />
     </div>
   );
 });
